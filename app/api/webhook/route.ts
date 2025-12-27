96e14be15d84675b40464cb153fccc8e0f38f0ad
Paramvir Singh Thind <paramvirsingh2540@gmail.com>
2025-12-27 00:22:27 -0800

    updated clerk  webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,3 +1,5 @@
+import { prisma } from "@/lib/prisma";
+import { UserJSON } from "@clerk/nextjs/server";
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 
@@ -5,14 +7,50 @@ export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req);
 
-    // Do something with payload
-    // For this guide, log payload to console
-    const { id } = evt.data;
+    const data = evt.data as UserJSON;
+    const { id, first_name, last_name, username, image_url, email_addresses } =
+      data;
     const eventType = evt.type;
-    console.log(
-      `Received webhook with ID ${id} and event type of ${eventType}`
-    );
-    console.log("Webhook payload:", evt.data);
+
+    if (evt.type === "user.created") {
+      await prisma.user.create({
+        data: {
+          clerkId: id,
+          firstName: first_name || "",
+          lastName: last_name,
+          username: username || "",
+          email: email_addresses[0].email_address,
+          profileUrl: image_url,
+        },
+      });
+    }
+
+    if (evt.type === "user.updated") {
+      await prisma.user.update({
+        where: {
+          clerkId: id,
+        },
+        data: {
+          firstName: first_name || "",
+          lastName: last_name,
+          username: username || "",
+          email: email_addresses[0].email_address,
+          profileUrl: image_url,
+        },
+      });
+    }
+
+    if (evt.type === "user.deleted") {
+      await prisma.user.delete({
+        where: {
+          clerkId: id,
+        },
+      });
+    }
+    // console.log(
+    //   `Received webhook with ID ${id}  and event type of ${eventType}`
+    // );
+    // console.log("Webhook payload:", evt.data);
 
     return new Response("Webhook received", { status: 200 });
   } catch (err) {
